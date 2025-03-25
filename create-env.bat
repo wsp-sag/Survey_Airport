e1a678160eeea82c36d6a95eeb9c22e525bcc3ad
mwe-sandag <114246988+mwe-sandag@users.noreply.github.com>
2025-03-24 18:33:50 -0700

    Update create-env.bat
    Use Windows %USERNAME% Variable

--- create-env.bat
+++ create-env.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Users\USVV724227\AppData\Local\Programs\Python\Python312;%PATH%
+set PATH=C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312;%PATH%
 python -m venv sandag
 call sandag\Scripts\activate
 pip install -r requirements.txt
